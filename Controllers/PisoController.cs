5587fd0eb580502a5ff69da03e5e2961da762fc2
luiz <luizmarcelolm@hotmail.com>
2023-07-05 22:20:34 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/PisoController.cs
@@ -0,0 +1,83 @@
+﻿using CalculadoraOnlineDePiso.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraOnlineDePiso.Controllers
+{
+    public class PisoController : Controller
+    {
+        private static List<Piso> pisos = new List<Piso>(); // Lista para armazenar os produtos
+
+        public ActionResult Index()
+        {
+            return View(pisos); // Retorna a lista de produtos para a view Index
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Piso piso)
+        {
+            if (ModelState.IsValid)
+            {
+                piso.Id = pisos.Count + 1;
+                pisos.Add(piso);
+                return RedirectToAction("Index");
+            }
+
+            return View(piso);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var produto = pisos.FirstOrDefault(p => p.Id == id);
+
+            if (produto == null)
+                return HttpNotFound();
+
+            return View(produto);
+        }
+
+        private ActionResult HttpNotFound()
+        {
+            return View(Index);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Piso piso)
+        {
+            if (ModelState.IsValid)
+            {
+                var pisoExistente = pisos.FirstOrDefault(p => p.Id == piso.Id);
+
+                if (pisoExistente == null)
+                    return HttpNotFound();
+
+                pisoExistente.Local = piso.Local;
+                pisoExistente.Area1 = piso.Area1;
+                pisoExistente.Area2 = piso.Area2;
+                pisoExistente.Piso1 = piso.Piso1;
+                pisoExistente.Piso1 = piso.Piso1;
+
+                return RedirectToAction("Index");
+            }
+
+            return View(piso);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var piso = pisos.FirstOrDefault(p => p.Id == id);
+
+            if (piso == null)
+                return HttpNotFound();
+
+            pisos.Remove(piso);
+
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Models/Piso.cs
@@ -0,0 +1,13 @@
+﻿namespace CalculadoraOnlineDePiso.Models
+{
+    public class Piso
+    {
+        public int Id { get; set; }
+        public string Local { get; set; }
+        public int Area1 { get; set; }
+        public int Area2 { get; set; }
+        public int Piso1 { get; set; }
+        public int Piso2 { get; set; }
+       
+    }
+}
