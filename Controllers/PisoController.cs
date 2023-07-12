7af56178bf31b3475c5a84c2183221a012988c24
luiz <luizmarcelolm@hotmail.com>
2023-07-11 22:24:04 -0300

    criação de opçao

--- Controllers/PisoController.cs
+++ Controllers/PisoController.cs
@@ -3,6 +3,7 @@ using System.Globalization;
 using System.Reflection;
 using CalculadoraOnlineDePiso.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace CalculadoraOnlineDePiso.Controllers
 {
@@ -25,19 +26,42 @@ namespace CalculadoraOnlineDePiso.Controllers
         {
             if (ModelState.IsValid)
             {
-                piso.CalculoArea = (piso.Area1 * piso.Area2);
-                piso.CalculoPiso = (piso.CalculoArea / ((piso.Piso1 / 100) * (piso.Piso2 / 100)));
-                piso.CalculoPiso = (piso.CalculoPiso + ((piso.CalculoPiso * 10) / 100));
-                piso.CalculoPiso = (float)Math.Round(piso.CalculoPiso);
+                if (piso.Opcao == "n")
+                {
+                    piso.CalculoArea = (piso.Area1 * piso.Area2);
+                    piso.CalculoArea = (float)Math.Round(piso.CalculoArea, 2);
+                    piso.CalculoAreaPiso = (piso.Piso1 * piso.Piso2) / 100;
+                    piso.CalculoAreaPiso = (float)Math.Round(piso.CalculoAreaPiso, 2);
+                    piso.CalculoPiso = piso.CalculoArea / piso.CalculoAreaPiso;
+                    piso.CalculoPiso = (float)Math.Round(piso.CalculoPiso);
+
+                    piso.Id = pisos.Count + 1;
+                    pisos.Add(piso);
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    piso.CalculoArea = (piso.Area1 * piso.Area2);
+                    piso.CalculoArea = (float)Math.Round(piso.CalculoArea, 2);
+                    piso.CalculoAreaPiso = ((piso.Piso1 * piso.Piso2) / 100);
+                    piso.CalculoAreaPiso = (float)Math.Round(piso.CalculoAreaPiso, 2);
+                    piso.CalculoPiso = piso.CalculoArea / piso.CalculoAreaPiso;
+                    piso.CalculoPiso = piso.CalculoPiso + ((piso.CalculoAreaPiso * 10) / 100);
+                    piso.CalculoPiso = (float)Math.Round(piso.CalculoPiso);
+                    piso.CalculoQuant = (float)Math.Round(piso.CalculoArea / piso.CalculoPiso);
+
+                    piso.Id = pisos.Count + 1;
+                    pisos.Add(piso);
+                    return RedirectToAction("Index");
+                }
 
 
-                piso.Id = pisos.Count + 1;
-                pisos.Add(piso);
-                return RedirectToAction("Index");
             }
-            return View(piso);
-        }
+                return View(piso);
+            
 
+        }
         public ActionResult Edit(int id)
         {
             var produto = pisos.FirstOrDefault(p => p.Id == id);
@@ -64,8 +88,11 @@ namespace CalculadoraOnlineDePiso.Controllers
                 pisoExistente.Piso1 = piso.Piso1;
                 pisoExistente.Piso2 = piso.Piso2;
                 pisoExistente.CalculoArea = (pisoExistente.Area1 * pisoExistente.Area2);
-                pisoExistente.CalculoPiso = (pisoExistente.CalculoArea / ((pisoExistente.Piso1 / 100) * (pisoExistente.Piso2 / 100)));
-               // pisoExistente.CalculoPiso = (pisoExistente.CalculoPiso + ((pisoExistente.CalculoPiso * 10) / 100));
+                pisoExistente.CalculoArea = (float)Math.Round(pisoExistente.CalculoArea, 2);
+                pisoExistente.CalculoAreaPiso = (pisoExistente.Piso1 / 100) * (pisoExistente.Piso2 / 100);
+                pisoExistente.CalculoAreaPiso = (float)Math.Round(pisoExistente.CalculoAreaPiso, 2);
+                // pisoExistente.CalculoPiso = (pisoExistente.CalculoPiso + ((pisoExistente.CalculoPiso * 10) / 100));
+                pisoExistente.CalculoPiso = pisoExistente.CalculoArea / pisoExistente.CalculoAreaPiso;
                 pisoExistente.CalculoPiso = (float)Math.Round(pisoExistente.CalculoPiso);
 
                 return RedirectToAction("Index");
--- Models/Piso.cs
+++ Models/Piso.cs
@@ -4,13 +4,15 @@
     {
         public int Id { get; set; }
         public string Local { get; set; }
-        public int Area1 { get; set; }
-        public int Area2 { get; set; }
+        public float Area1 { get; set; }
+        public float Area2 { get; set; }
         public float Piso1 { get; set; }
         public float Piso2 { get; set; }
         public float CalculoArea { get; set; }
         public float CalculoPiso { get; set; }
-
+        public float CalculoAreaPiso { get; set; }
+        public float CalculoQuant { get; set; }
+        public string Opcao { get; set; }
 
     }
 }
