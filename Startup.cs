509d65be79345291739b72d4de0e538c0fb40eac
luiz <luizmarcelolm@hotmail.com>
2023-07-22 15:38:00 -0300

    alteração startup

--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,7 @@ namespace CalculadoraOnlineDePiso
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Piso/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -49,7 +49,7 @@ namespace CalculadoraOnlineDePiso
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Piso}/{action=Index}/{id?}");
 
         }
 
